e209fda0225bad8cb8b6a37639780de09b54895e
PABLOSleuth2 <opsnonman@gmail.com>
2023-08-20 17:55:51 +0600

    Add files via upload

--- RobloxScripts/TEST/source.lua
+++ RobloxScripts/TEST/source.lua
@@ -164,23 +164,24 @@ end
 
 PARENT = nil
 if get_hidden_gui or gethui then
+    local Players = game:GetService("Players")
 	local hiddenUI = get_hidden_gui or gethui
 	local Main = Instance.new("ScreenGui")
 	Main.Name = "Rayfield"
-	Main.Parent = hiddenUI()
+	Main.Parent = Players.LocalPlayer.PlayerGui
 	PARENT = Main
 elseif (not is_sirhurt_closure) and (syn and syn.protect_gui) then
 	local Main = Instance.new("ScreenGui")
 	Main.Name = "Rayfield"
 	syn.protect_gui(Main)
-	Main.Parent = COREGUI
+	Main.Parent = Players.LocalPlayer.PlayerGui
 	PARENT = Main
 elseif COREGUI:FindFirstChild('RobloxGui') then
 	PARENT = COREGUI.RobloxGui
 else
 	local Main = Instance.new("ScreenGui")
 	Main.Name = "Rayfield"
-	Main.Parent = COREGUI
+	Main.Parent = Players.LocalPlayer.PlayerGui
 	PARENT = Main
 end
 
